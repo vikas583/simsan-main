b11cb6a25e45d4385786597777db2a48da1b1cd5
vikas96-saxecap <vikas@saxecap.com>
2025-12-08 23:37:46 +0530

    changes

--- next.config.ts
+++ next.config.ts
@@ -9,8 +9,6 @@ const nextConfig: NextConfig = {
   },
   // Enable compression
   compress: true,
-  // Optimize production builds
-  swcMinify: true,
   // Reduce bundle size
   experimental: {
     optimizePackageImports: ["@mui/material", "@mui/icons-material"],
